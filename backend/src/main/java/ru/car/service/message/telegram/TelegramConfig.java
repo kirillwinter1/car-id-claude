fded93b3dc2c215dba4d7c7200f0789aa3e2629c
Kirill Reshetov <kirillreshetov@MacBook-Air-Kirill.local>
2026-04-21 14:39:03 +0300

    refactor(tg-bot): simplify TelegramConfig (no setter bootstrap)

--- backend/src/main/java/ru/car/service/message/telegram/TelegramConfig.java
+++ backend/src/main/java/ru/car/service/message/telegram/TelegramConfig.java
@@ -4,19 +4,15 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import ru.car.service.NotificationFacade;
 
 @Configuration
 @RequiredArgsConstructor
 public class TelegramConfig {
 
     private final TelegramBotService telegramBotService;
-    private final TelegramLogicService telegramLogicService;
-
 
     @PostConstruct
-    void initMessageService() throws TelegramApiException {
-        telegramBotService.setTelegramLogicService(telegramLogicService);
+    void initTelegramBot() throws TelegramApiException {
         telegramBotService.init();
     }
 }
